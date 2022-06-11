9fed9b7a56f81878f20500af3ba8a1f3c15af089
Jayesh Nirve <nirvejayesh@gmail.com>
2022-06-11 20:55:57 +0530

    feat: get status of current activity

--- android/src/main/kotlin/com/pravera/flutter_foreground_task/MethodCallHandlerImpl.kt
+++ android/src/main/kotlin/com/pravera/flutter_foreground_task/MethodCallHandlerImpl.kt
@@ -59,6 +59,7 @@ class MethodCallHandlerImpl(private val context: Context, private val provider: 
 					}
 				}
 			}
+			"isAppOnForeground" -> result.success(ForegroundServiceUtils.isAppOnForeground(activity))
 			"setOnLockScreenVisibility" -> {
 				val arguments: Map<String, Any> = call.arguments as Map<String, Any>
 				val isVisible = arguments["isVisible"] as Boolean
