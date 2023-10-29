5a6e8aec4d9839bff70626bdbc29a30142eedd59
rajan-nonstopio <rajan.metaliya@nonstopio.com>
2023-10-29 10:25:32 +0530

    added ACTION_BOOT_COMPLETED

--- android/src/main/kotlin/com/pravera/flutter_foreground_task/service/BootReceiver.kt
+++ android/src/main/kotlin/com/pravera/flutter_foreground_task/service/BootReceiver.kt
@@ -3,6 +3,7 @@ package com.pravera.flutter_foreground_task.service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.pravera.flutter_foreground_task.models.ForegroundServiceAction
 import com.pravera.flutter_foreground_task.models.ForegroundServiceStatus
@@ -25,6 +26,27 @@ class BootReceiver : BroadcastReceiver() {
             val nIntent = Intent(context, ForegroundService::class.java)
             ForegroundServiceStatus.putData(context, ForegroundServiceAction.REBOOT)
             ContextCompat.startForegroundService(context, nIntent)
+            Toast.makeText(context, "Boot completed 1", Toast.LENGTH_LONG).show();
+        }
+
+        if (context != null && intent?.action == "android.intent.action.ACTION_BOOT_COMPLETED") {
+            // Check whether to start the service at boot time.
+            val options = ForegroundTaskOptions.getData(context)
+            if (!options.autoRunOnBoot) return
+
+            // Create an intent for calling the service and store the action to be executed.
+            val nIntent = Intent(context, ForegroundService::class.java)
+            ForegroundServiceStatus.putData(context, ForegroundServiceAction.REBOOT)
+            ContextCompat.startForegroundService(context, nIntent)
+
+
+            // Show toast message
+            Toast.makeText(context, "Boot completed 2", Toast.LENGTH_LONG).show();
+
+        }
+
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
+
         }
     }
 }
