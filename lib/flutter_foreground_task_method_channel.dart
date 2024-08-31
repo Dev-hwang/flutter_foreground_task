79a11926c87a5406510a4b7905a80a897e12b525
wjhwang <hwj930513@naver.com>
2024-08-31 19:50:45 +0900

    refactor: Refactor dart-side code for testing

--- lib/flutter_foreground_task_method_channel.dart
+++ lib/flutter_foreground_task_method_channel.dart
@@ -1,105 +1,70 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:developer' as dev;
 import 'dart:io';
 import 'dart:ui';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 import 'flutter_foreground_task_platform_interface.dart';
-import 'errors/service_timeout_exception.dart';
 import 'models/android_notification_options.dart';
 import 'models/foreground_task_options.dart';
 import 'models/ios_notification_options.dart';
 import 'models/notification_button.dart';
 import 'models/notification_icon_data.dart';
 import 'models/notification_permission.dart';
-import 'models/service_request_result.dart';
+import 'task_handler.dart';
+
+const MethodChannel _kMDChannel =
+    MethodChannel('flutter_foreground_task/methods');
+const MethodChannel _kBGChannel =
+    MethodChannel('flutter_foreground_task/background');
 
 /// An implementation of [FlutterForegroundTaskPlatform] that uses method channels.
 class MethodChannelFlutterForegroundTask extends FlutterForegroundTaskPlatform {
-  /// The method channel used to interact with the native platform.
-  @visibleForTesting
-  final methodChannel = const MethodChannel('flutter_foreground_task/methods');
+  // ====================== Service ======================
 
   @override
-  Future<ServiceRequestResult> startService({
-    int? serviceId,
+  Future<void> startService({
     required AndroidNotificationOptions androidNotificationOptions,
     required IOSNotificationOptions iosNotificationOptions,
     required ForegroundTaskOptions foregroundTaskOptions,
+    int? serviceId,
     required String notificationTitle,
     required String notificationText,
     NotificationIconData? notificationIcon,
     List<NotificationButton>? notificationButtons,
     Function? callback,
   }) async {
-    // Notification Permission for Android 13+
-    if (Platform.isAndroid && await attachedActivity) {
-      try {
-        final NotificationPermission notificationPermissionStatus =
-            await checkNotificationPermission();
-        if (notificationPermissionStatus != NotificationPermission.granted) {
-          await requestNotificationPermission();
-        }
-      } catch (_) {
-        //
-      }
+    final Map<String, dynamic> options = {
+      'serviceId': serviceId,
+      if (Platform.isAndroid)
+        ...androidNotificationOptions.toJson()
+      else
+        ...iosNotificationOptions.toJson(),
+      ...foregroundTaskOptions.toJson(),
+      'notificationContentTitle': notificationTitle,
+      'notificationContentText': notificationText,
+      'iconData': notificationIcon?.toJson(),
+      'buttons': notificationButtons?.map((e) => e.toJson()).toList()
+    };
+
+    if (callback != null) {
+      options['callbackHandle'] =
+          PluginUtilities.getCallbackHandle(callback)?.toRawHandle();
     }
 
-    try {
-      final options = <String, dynamic>{
-        'serviceId': serviceId,
-        if (Platform.isAndroid)
-          ...androidNotificationOptions.toJson()
-        else
-          ...iosNotificationOptions.toJson(),
-        ...foregroundTaskOptions.toJson(),
-        'notificationContentTitle': notificationTitle,
-        'notificationContentText': notificationText,
-        'iconData': notificationIcon?.toJson(),
-        'buttons': notificationButtons?.map((e) => e.toJson()).toList()
-      };
-      if (callback != null) {
-        options['callbackHandle'] =
-            PluginUtilities.getCallbackHandle(callback)?.toRawHandle();
-      }
-      await methodChannel.invokeMethod('startService', options);
-    } catch (e) {
-      return ServiceRequestResult.error(e);
-    }
-
-    final Stopwatch stopwatch = Stopwatch()..start();
-    bool isStarted = false;
-    await Future.doWhile(() async {
-      isStarted = await isRunningService;
-
-      // official doc: Once the service has been created, the service must call its startForeground() method within five seconds.
-      // ref: https://developer.android.com/guide/components/services#StartingAService
-      if (isStarted || stopwatch.elapsedMilliseconds > 5 * 1000) {
-        return false;
-      } else {
-        await Future.delayed(const Duration(milliseconds: 100));
-        return true;
-      }
-    });
-
-    return isStarted
-        ? ServiceRequestResult.success()
-        : ServiceRequestResult.error(ServiceTimeoutException());
+    await _kMDChannel.invokeMethod('startService', options);
   }
 
   @override
-  Future<ServiceRequestResult> restartService() async {
-    try {
-      await methodChannel.invokeMethod('restartService');
-      return ServiceRequestResult.success();
-    } catch (e) {
-      return ServiceRequestResult.error(e);
-    }
+  Future<void> restartService() async {
+    await _kMDChannel.invokeMethod('restartService');
   }
 
   @override
-  Future<ServiceRequestResult> updateService({
+  Future<void> updateService({
     ForegroundTaskOptions? foregroundTaskOptions,
     String? notificationTitle,
     String? notificationText,
@@ -107,85 +72,111 @@ class MethodChannelFlutterForegroundTask extends FlutterForegroundTaskPlatform {
     List<NotificationButton>? notificationButtons,
     Function? callback,
   }) async {
-    try {
-      final options = <String, dynamic>{
-        if (foregroundTaskOptions != null) ...foregroundTaskOptions.toJson(),
-        'notificationContentTitle': notificationTitle,
-        'notificationContentText': notificationText,
-        'iconData': notificationIcon?.toJson(),
-        'buttons': notificationButtons?.map((e) => e.toJson()).toList()
-      };
-      if (callback != null) {
-        options['callbackHandle'] =
-            PluginUtilities.getCallbackHandle(callback)?.toRawHandle();
-      }
-      await methodChannel.invokeMethod('updateService', options);
-      return ServiceRequestResult.success();
-    } catch (e) {
-      return ServiceRequestResult.error(e);
+    final Map<String, dynamic> options = {
+      if (foregroundTaskOptions != null) ...foregroundTaskOptions.toJson(),
+      'notificationContentTitle': notificationTitle,
+      'notificationContentText': notificationText,
+      'iconData': notificationIcon?.toJson(),
+      'buttons': notificationButtons?.map((e) => e.toJson()).toList()
+    };
+
+    if (callback != null) {
+      options['callbackHandle'] =
+          PluginUtilities.getCallbackHandle(callback)?.toRawHandle();
     }
+
+    await _kMDChannel.invokeMethod('updateService', options);
   }
 
   @override
-  Future<ServiceRequestResult> stopService() async {
-    try {
-      await methodChannel.invokeMethod('stopService');
-    } catch (e) {
-      return ServiceRequestResult.error(e);
-    }
-
-    final Stopwatch stopwatch = Stopwatch()..start();
-    bool isStopped = false;
-    await Future.doWhile(() async {
-      isStopped = !await isRunningService;
-
-      // official doc: Once the service has been created, the service must call its startForeground() method within five seconds.
-      // ref: https://developer.android.com/guide/components/services#StartingAService
-      if (isStopped || stopwatch.elapsedMilliseconds > 5 * 1000) {
-        return false;
-      } else {
-        await Future.delayed(const Duration(milliseconds: 100));
-        return true;
-      }
-    });
-
-    return isStopped
-        ? ServiceRequestResult.success()
-        : ServiceRequestResult.error(ServiceTimeoutException());
-  }
-
-  @override
-  void sendData(Object data) {
-    methodChannel.invokeMethod('sendData', data);
+  Future<void> stopService() async {
+    await _kMDChannel.invokeMethod('stopService');
   }
 
   @override
   Future<bool> get isRunningService async {
-    return await methodChannel.invokeMethod('isRunningService');
+    return await _kMDChannel.invokeMethod('isRunningService');
   }
 
   @override
   Future<bool> get attachedActivity async {
     if (Platform.isAndroid) {
-      return await methodChannel.invokeMethod('attachedActivity');
+      return await _kMDChannel.invokeMethod('attachedActivity');
     }
     return true;
   }
 
   @override
-  void minimizeApp() => methodChannel.invokeMethod('minimizeApp');
+  void setTaskHandler(TaskHandler handler) {
+    // Binding the framework to the flutter engine.
+    WidgetsFlutterBinding.ensureInitialized();
+    DartPluginRegistrant.ensureInitialized();
+
+    // Set the method call handler for the background channel.
+    _kBGChannel.setMethodCallHandler((call) async {
+      final DateTime timestamp = DateTime.timestamp();
+
+      switch (call.method) {
+        case 'onStart':
+          handler.onStart(timestamp);
+          break;
+        case 'onRepeatEvent':
+          handler.onRepeatEvent(timestamp);
+          break;
+        case 'onDestroy':
+          handler.onDestroy(timestamp);
+          break;
+        case 'onReceiveData':
+          dynamic data = call.arguments;
+          if (data is List || data is Map) {
+            try {
+              data = jsonDecode(jsonEncode(data));
+            } catch (e, s) {
+              dev.log('onReceiveData error: $e\n$s');
+            }
+          }
+          handler.onReceiveData(data);
+          break;
+        case 'onNotificationButtonPressed':
+          final String id = call.arguments.toString();
+          handler.onNotificationButtonPressed(id);
+          break;
+        case 'onNotificationDismissed':
+          handler.onNotificationDismissed();
+          break;
+        case 'onNotificationPressed':
+          handler.onNotificationPressed();
+      }
+    });
+
+    _kBGChannel.invokeMethod('startTask');
+  }
+
+  // =================== Communication ===================
+
+  @override
+  void sendDataToTask(Object data) {
+    _kMDChannel.invokeMethod('sendData', data);
+  }
+
+  // ====================== Utility ======================
+
+  @override
+  void minimizeApp() {
+    _kMDChannel.invokeMethod('minimizeApp');
+  }
 
   @override
   void launchApp([String? route]) {
     if (Platform.isAndroid) {
-      methodChannel.invokeMethod('launchApp', route);
+      _kMDChannel.invokeMethod('launchApp', route);
     }
   }
 
   @override
   void setOnLockScreenVisibility(bool isVisible) {
     if (Platform.isAndroid) {
-      methodChannel.invokeMethod('setOnLockScreenVisibility', {
+      _kMDChannel.invokeMethod('setOnLockScreenVisibility', {
         'isVisible': isVisible,
       });
     }
@@ -193,20 +184,20 @@ class MethodChannelFlutterForegroundTask extends FlutterForegroundTaskPlatform {
 
   @override
   Future<bool> get isAppOnForeground async {
-    return await methodChannel.invokeMethod('isAppOnForeground');
+    return await _kMDChannel.invokeMethod('isAppOnForeground');
   }
 
   @override
   void wakeUpScreen() {
     if (Platform.isAndroid) {
-      methodChannel.invokeMethod('wakeUpScreen');
+      _kMDChannel.invokeMethod('wakeUpScreen');
     }
   }
 
   @override
   Future<bool> get isIgnoringBatteryOptimizations async {
     if (Platform.isAndroid) {
-      return await methodChannel.invokeMethod('isIgnoringBatteryOptimizations');
+      return await _kMDChannel.invokeMethod('isIgnoringBatteryOptimizations');
     }
     return true;
   }
@@ -214,7 +205,7 @@ class MethodChannelFlutterForegroundTask extends FlutterForegroundTaskPlatform {
   @override
   Future<bool> openIgnoreBatteryOptimizationSettings() async {
     if (Platform.isAndroid) {
-      return await methodChannel
+      return await _kMDChannel
           .invokeMethod('openIgnoreBatteryOptimizationSettings');
     }
     return true;
@@ -223,8 +214,7 @@ class MethodChannelFlutterForegroundTask extends FlutterForegroundTaskPlatform {
   @override
   Future<bool> requestIgnoreBatteryOptimization() async {
     if (Platform.isAndroid) {
-      return await methodChannel
-          .invokeMethod('requestIgnoreBatteryOptimization');
+      return await _kMDChannel.invokeMethod('requestIgnoreBatteryOptimization');
     }
     return true;
   }
@@ -232,7 +222,7 @@ class MethodChannelFlutterForegroundTask extends FlutterForegroundTaskPlatform {
   @override
   Future<bool> get canDrawOverlays async {
     if (Platform.isAndroid) {
-      return await methodChannel.invokeMethod('canDrawOverlays');
+      return await _kMDChannel.invokeMethod('canDrawOverlays');
     }
     return true;
   }
@@ -240,7 +230,7 @@ class MethodChannelFlutterForegroundTask extends FlutterForegroundTaskPlatform {
   @override
   Future<bool> openSystemAlertWindowSettings() async {
     if (Platform.isAndroid) {
-      return await methodChannel.invokeMethod('openSystemAlertWindowSettings');
+      return await _kMDChannel.invokeMethod('openSystemAlertWindowSettings');
     }
     return true;
   }
@@ -248,21 +238,21 @@ class MethodChannelFlutterForegroundTask extends FlutterForegroundTaskPlatform {
   @override
   Future<NotificationPermission> checkNotificationPermission() async {
     final int result =
-        await methodChannel.invokeMethod('checkNotificationPermission');
-    return getNotificationPermissionFromIndex(result);
+        await _kMDChannel.invokeMethod('checkNotificationPermission');
+    return NotificationPermission.fromIndex(result);
   }
 
   @override
   Future<NotificationPermission> requestNotificationPermission() async {
     final int result =
-        await methodChannel.invokeMethod('requestNotificationPermission');
-    return getNotificationPermissionFromIndex(result);
+        await _kMDChannel.invokeMethod('requestNotificationPermission');
+    return NotificationPermission.fromIndex(result);
   }
 
   @override
   Future<bool> get canScheduleExactAlarms async {
     if (Platform.isAndroid) {
-      return await methodChannel.invokeMethod('canScheduleExactAlarms');
+      return await _kMDChannel.invokeMethod('canScheduleExactAlarms');
     }
     return true;
   }
@@ -270,7 +260,7 @@ class MethodChannelFlutterForegroundTask extends FlutterForegroundTaskPlatform {
   @override
   Future<bool> openAlarmsAndRemindersSettings() async {
     if (Platform.isAndroid) {
-      return await methodChannel.invokeMethod('openAlarmsAndRemindersSettings');
+      return await _kMDChannel.invokeMethod('openAlarmsAndRemindersSettings');
     }
     return true;
   }
